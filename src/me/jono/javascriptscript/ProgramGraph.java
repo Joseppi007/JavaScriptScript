0b2f742c8c7ed04ebcad15e40ededc38048281b8
Joseph Novack <joeynovack314@gmail.com>
2022-09-28 10:53:33 -0400

    Stuff is still broken, but now there's more half-baked stuff!

--- src/me/jono/javascriptscript/ProgramGraph.java
+++ src/me/jono/javascriptscript/ProgramGraph.java
@@ -20,9 +20,41 @@ public class ProgramGraph {
 
     /**
      * Loads a program from a file
+     * <b><i><u>Yet to be implemented</u></i></b>
      * @param srcCode The source code
      */
     public ProgramGraph(File srcCode) {
+        nodes = new HashMap<>();
+    }
 
+    /**
+     * Adds a Node to the ProgramGraph.
+     * The Node must have a unique name.
+     * Use nameNode to replace a Node's name with a unique name.
+     * @param node the Node to add
+     */
+    public void addNode(Node node) {
+        assert(nodes.containsKey(node.getName()));
+    }
+
+    /**
+     * Replaces the name of a node with a randomly generated String
+     * @param node the Node to rename
+     */
+    public void nameNode(Node node) {
+
+    }
+
+    /**
+     * Makes a pseudo-random String of the given length
+     * @param length The length to make the String
+     * @return A pseudo-random String
+     */
+    public static String randomString(int length) {
+        StringBuilder r = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            r.append(Character.toString(65 + (int) Math.floor(26.0 * Math.random())));
+        }
+        return r.toString();
     }
 }
